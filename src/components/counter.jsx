eea6b60b547e318ee8e3db4542cb29696021c494
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2022-07-08 05:53:29 +0000

    Another one ! 🤠

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-     count : 0,
+     count : 1,
+     
     }
+     
+    
+
     render() { 
+        let colors = "badge m-2 badge-";
+     colors += this.state.count === 0 ? "warning" : "primary"; 
         return <React.Fragment> 
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span  className={colors}>{this.formatCount()}</span>
         <button className="btn btn-secondary">Increment</button>
          </React.Fragment>;
     }
@@ -15,6 +21,8 @@ class Counter extends Component {
         const {count} = this.state;
         return count === 0 ? "zero" : count 
     }
+
+    
 }
  
 export default Counter;

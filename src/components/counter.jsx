ae35d2cdd3a5dfdbffb3b14525ab11bb32be8720
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2022-07-12 22:34:48 +0000

    Another one ! 👻

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,31 +2,33 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-     count : 0,
+      count: 0,  
     }
-     
-    styles = {
-        fontSize:18,
+    setIncrement=()=>{
+     this.setState({count: this.state.count+1});
     }
-
+    fontChange = {
+      fontSize:18,   
+    }
     render() { 
-         return <React.Fragment> 
-        <span style={this.styles} className={this.changeColor()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary">Increment</button>
-         </React.Fragment>;
+        return (
+            <React.Fragment>
+            <span style={this.fontChange} className={this.changeColor()}>{this.checkStatus()}</span>
+            <button onClick={this.setIncrement}  className="btn btn-secondary">Increment</button>
+            </React.Fragment>
+            );
     }
 
-    changeColor(){
-        let colors = "badge m-2 badge-";
-        colors += this.state.count === 0 ? "warning" : "primary";
-        return colors;
+    checkStatus(){
+       const count = this.state.count;
+        return count === 0 ? "zero" : count;
     }
-    formatCount(){
-        const {count} = this.state;
-        return count === 0 ? "zero" : count 
-    }
-
     
+    changeColor(){
+        let change = "badge  m-2 badge-"; 
+        change += this.state.count === 0 ? "warning" : "primary";
+        return change;
+    }
 }
  
 export default Counter;
